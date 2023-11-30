d03111aa37399a7a251e96a9b9aa71fc20349eb7
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-30 23:27:02 +0300

    react scss

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc03_sass/src/components/Card.jsx
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc03_sass/src/components/Card.jsx
@@ -4,10 +4,17 @@ const Card = ({ veri }) => {
   // const { id, name, job, comment } = veri[0];
   return (
     <div>
-      {veri.map(({ id, name, job, comment }) => (
+      {veri.map(({ id, name, job, comment,img }) => (
         <div key={id}>
           <h2> {name} </h2>
-          
+          <h3>{job} </h3>
+          <p> {comment} </p>
+          <img src={img} alt="" width={"100px"}/>
+          <div>
+            <button>Small</button>
+            <button>Large</button>
+          </div>
+
         </div>
       ))}
     </div>
