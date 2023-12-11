d6682311a5116fb3931ac7b075678eceaf3a0005
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-11 21:24:25 +0300

    modal dr name add

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc08_appointment-app/appointment-app-inclass/src/components/Doctors.jsx
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc08_appointment-app/appointment-app-inclass/src/components/Doctors.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import { doctorData } from "../helper/data";
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import AddModal from "./AddModal";
 const Doctors = () => {
+  const [show, setShow] = useState(false);
+  const[drName,setDrName]=useState("")
+
+  const handleClose = () => setShow(false);
+  const handleShow = (name) =>{
+    setShow(true);
+    setDrName(name)
+  } 
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
       <Row>
-{doctorData.map(({id,name,img,dep})=>(
-  <Col key={id}>
-    <img src={img} alt={name} className="img-thumbnail doctor-img" />
-    <h5> {name} </h5>
-    <h6> {dep} </h6>
-  </Col>
-))}
+        {doctorData.map(({ id, name, img, dep }) => (
+          <Col key={id}>
+            <img
+              src={img}
+              alt={name}
+              className="img-thumbnail doctor-img"
+              onClick={()=>handleShow(name) }
+            />
+            <h5> {name} </h5>
+            <h6> {dep} </h6>
+          </Col>
+        ))}
       </Row>
+      <AddModal handleClose={handleClose} show={show} drName={drName} />
     </Container>
   );
 };
