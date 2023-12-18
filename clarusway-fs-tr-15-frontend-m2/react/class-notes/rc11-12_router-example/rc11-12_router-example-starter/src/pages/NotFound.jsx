45056372640b3ee7e04047da067db9b22f528f19
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-18 15:52:18 +0300

    react router active navigate

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc11-12_router-example/rc11-12_router-example-starter/src/pages/NotFound.jsx
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc11-12_router-example/rc11-12_router-example-starter/src/pages/NotFound.jsx
@@ -1,12 +1,17 @@
 import errorImg from "../img/404.png";
-
+import { useNavigate, useParams } from "react-router-dom";
 const NotFound = () => {
+    const navigate = useNavigate();
   return (
     <div className="container text-center mt-4">
       <img className="w-75" src={errorImg} alt="" />
       <div>
-        <button className="btn btn-success me-2">Home</button>
-        <button className="btn btn-warning">Go Back</button>
+        <button className="btn btn-warning me-2" onClick={() => navigate(-1)}>
+          Back
+        </button>
+        <button className="btn btn-danger" onClick={() => navigate("/")}>
+          Back
+        </button>
       </div>
     </div>
   );
