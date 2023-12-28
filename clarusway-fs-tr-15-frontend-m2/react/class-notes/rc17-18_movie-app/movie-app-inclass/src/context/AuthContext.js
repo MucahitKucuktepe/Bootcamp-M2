30d03433f2a7b95156bdad316b997a9d019b7caf
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-28 09:04:44 +0300

    bootcamp projets added

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-inclass/src/context/AuthContext.js
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-inclass/src/context/AuthContext.js
@@ -1,12 +1,27 @@
 import React, { Children, createContext, useContext } from "react";
+import { auth } from "../auth/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 export const AuthContext = createContext();
 export const useAuthContext = () => {
   return useContext(AuthContext);
 };
 const AuthContextProvider = ({ children }) => {
-  const values = {};
+  const createUser = async (email, password) => {
+    try {
+      //?yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const values = { createUser };
   return (
-    <AuthContext.Provider value={values}>{Children} </AuthContext.Provider>
+    <AuthContext.Provider value={values}>{children} </AuthContext.Provider>
   );
 };
 
