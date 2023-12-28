3684268cc33599074349295086ae415addaa7bf8
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-28 16:08:50 +0300

    context

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/auth/firebase.js
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/auth/firebase.js
@@ -4,17 +4,16 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyBZ75HYTenMTuYTpblR_rTdbZG4vRd_oCA",
-    authDomain: "movie-app-ad9a5.firebaseapp.com",
-    projectId: "movie-app-ad9a5",
-    storageBucket: "movie-app-ad9a5.appspot.com",
-    messagingSenderId: "964635118269",
-    appId: "1:964635118269:web:426d9b6e3705eb0f1f05d4"
-  };
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
