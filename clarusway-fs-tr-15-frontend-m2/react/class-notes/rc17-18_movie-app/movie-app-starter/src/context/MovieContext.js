2dde7c1bc060bf9ad193aa36d44787f9fb913708
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-29 18:49:42 +0300

    conditional classname vote_averagfe

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/context/MovieContext.js
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/context/MovieContext.js
@@ -1,16 +1,50 @@
-import React, { createContext, useContext } from "react";
-
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 export const MovieContext = createContext();
 export const useMovieContext = () => {
   return useContext(MovieContext);
 };
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 
 const MovieContextProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //   const getMovies = () => {
+  //     setLoading(true);
+  //     axios
+  //       .get(FEATURED_API)
+  //       .then((res) => setMovies(res.data.results))
+  //       .catch((err) => console.log(err))
+  //       .finally(() => setLoading(false));
+  //     console.log(movies);
+  //   };
+  //   useEffect(() => {
+  //     getMovies();
+  //   }, []);
+
+  const getMovies = async () => {
+    setLoading(true);
+    try {
+      const {data} = await axios(FEATURED_API);
+      console.log(data.results);
+      setMovies(data.results)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div>
-      <MovieContext.Provider value={value}> {children} </MovieContext.Provider>
-    </div>
+    <MovieContext.Provider value={{ loading, movies }}>
+      {" "}
+      {children}{" "}
+    </MovieContext.Provider>
   );
 };
-
 export default MovieContextProvider;
--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/pages/Main.jsx
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc17-18_movie-app/movie-app-starter/src/pages/Main.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Main = () => {
-  return <div>Main </div>;
+  const { movies, loading } = useMovieContext();
+  console.log(movies);
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center">
+        {loading ? (
+          <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52" role="status">
+            <span className="visualy-hidden">Loading</span>{" "}
+          </div>
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
