baad92183c69923db143886b00d3753adae617d9
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-04 21:26:58 +0300

    provider

---
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc21_redux-example/redux-toolkit-inClass/src/features/newsApiSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const newsApiSlice = createSlice({
+  name: "newsApi",
+  initialState,
+  reducers: {}
+});
+
+export const {} = newsApiSlice.actions
+
+export default newsApiSlice.reducer
