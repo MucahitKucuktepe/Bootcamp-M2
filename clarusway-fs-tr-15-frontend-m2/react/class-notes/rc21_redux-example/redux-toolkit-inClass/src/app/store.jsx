eac1d8a5fa300ce3df38084ba7b93f2c9bc62628
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-04 21:37:39 +0300

    devTool closed function

--- clarusway-fs-tr-15-frontend-m2/react/class-notes/rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import newsReducer from "../features/newsApiSlice";
 
-
-export const store=configureStore({
-    reducer:{
-        auth:authReducer,
-        newsApi: newsReducer
-    }
-})
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    newsApi: newsReducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  //? eger gelistirme asaması production is o zaman yukarıdaki ifade false dondurur ve dolayısyla devTool kullanıma kapalı olur
+});
