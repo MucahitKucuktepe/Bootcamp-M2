3487a7b9460424ac952dcec9d54ec7cd23fea86a
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-05 17:01:06 +0300

    store added

---
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc21_redux-example/redux-toolkit-starter/src/features/authSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: { email: "", password: "" },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: { //? {type,payload}=action
+    setUser: (state, action) => {
+      state.user=action.payload
+    },
+    clearUser: (state) => {
+        state.user={email:"",password:""}
+    },
+  },
+});
+
+export const {setUser,clearUser}=authSlice.actions
+export default authSlice.reducer
---
+++ clarusway-fs-tr-15-frontend-m2/react/class-notes/rc21_redux-example/redux-toolkit-starter/src/features/newsApiSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const newsApiSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = newsApiSlice.actions
+
+export default newsApiSlice.reducer
