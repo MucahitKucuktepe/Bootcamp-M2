b2ff399497f04ae5330612b6c531e1018cf64893
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-29 22:01:59 +0300

    feat: navigate system in next.js

---
+++ clarusway-fs-tr-15-frontend-m2/nextjs/class-notes/nc01/app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const DashboardLayout = ({children}) => {
+  return (
+    <div>
+    <Sidebar />
+    {children}</div>
+  )
+}
+
+export default DashboardLayout
--- clarusway-fs-tr-15-frontend-m2/nextjs/class-notes/nc01/app/page.js
+++ clarusway-fs-tr-15-frontend-m2/nextjs/class-notes/nc01/app/page.js
@@ -1,13 +1,13 @@
-import React from 'react'
+//? app folder içindeki page.js "/" route olan home sayfasıdır
+//? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
+import React from "react";
 const page = () => {
   return (
     <div>
-      <h1 className='text-2xl text-center'>
-        HOME
-      </h1>
+      <h1 className="text-2xl text-center">HOME</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
